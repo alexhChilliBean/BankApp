8b6eba9d10fe39b5b1bcc6ed0d62ecd8652a4518
Alex Holmes <alexholmes@alexs-mbp.home>
2021-10-08 15:50:07 +0100

    Login directs to login, logo directs to root

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#login'
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
-  get 'logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
