bb0e20ec805c5662dffc0818c8202cd53d59dfba
Alex Holmes <alexholmes@alexs-mbp.home>
2021-10-08 14:56:35 +0100

    Added Users and Sessions controllers and Users model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   root 'pages#index'
+
+  #Users routes
+  resources :users, only: [:new, :create, :edit, :update, :show, :destroy]
+
+  #Session routes
+  get '/login', to: 'sessions#login'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+  get 'logout', to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
