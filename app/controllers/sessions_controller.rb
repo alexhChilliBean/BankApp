68ebff59677f28ca2b57159e8836383f13ed70fa
Alex Holmes <alexholmes@alexs-mbp.home>
2021-10-11 11:02:08 +0100

    Added flash notifications to login/logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
 
         if !!@user && @user.authenticate(params[:password])
             session[:user_id] = @user.id
+            flash[:success] = "Welcome back!"
             redirect_to user_path(@user)
         else
             message = "Incorrect username or password"
