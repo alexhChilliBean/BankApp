ce953c84fe3108e4d2ee661de11823fa9aa8057f
Alex Holmes <alexholmes@alexs-mbp.home>
2021-10-13 12:30:01 +0100

    Created find_user action and new accounts view

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,14 @@
 class AccountsController < ApplicationController
+    before_action :find_user
+    
+    def new
+        @account = Account.new
+    end
+
+    private
+
+    def find_user
+        @user = User.find(params[:user_id])
+    end
+
 end
