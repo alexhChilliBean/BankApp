d07635ae804b533359d6720f60845a4620ddfbdc
Alex Holmes <alexholmes@Alexs-MacBook-Pro.local>
2021-10-14 15:51:57 +0100

    Validation on accounts

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,5 +2,6 @@ class Account < ApplicationRecord
     belongs_to :user
     validates :account_name, :account_no, :balance, presence: true
     validates :account_no, uniqueness: true
+    validates :account_name, uniqueness: {scope: :user_id}
 
 end
