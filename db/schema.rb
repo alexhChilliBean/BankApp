93db16fc788d92a3e6c5c2c2cbaecb1b08f3a9d1
Alex Holmes <alexholmes@Alexs-MacBook-Pro.local>
2021-10-14 16:29:44 +0100

    Created transactions model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_13_153336) do
+ActiveRecord::Schema.define(version: 2021_10_14_145510) do
 
   create_table "accounts", force: :cascade do |t|
     t.integer "account_no"
@@ -20,6 +20,17 @@ ActiveRecord::Schema.define(version: 2021_10_13_153336) do
     t.string "account_name"
     t.integer "user_id", null: false
     t.index ["user_id"], name: "index_accounts_on_user_id"
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.string "reference"
+    t.integer "amount"
+    t.integer "accounts_id"
+    t.integer "users_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["accounts_id"], name: "index_transactions_on_accounts_id"
+    t.index ["users_id"], name: "index_transactions_on_users_id"
   end
 
   create_table "users", force: :cascade do |t|
