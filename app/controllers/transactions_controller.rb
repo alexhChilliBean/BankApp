297da55df99894ea259b198804d72a483e985558
Alex Holmes <alexholmes@Alexs-MacBook-Pro.local>
2021-10-21 17:07:08 +0100

    Wrote the form to transfer money

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -7,14 +7,31 @@ class TransactionsController < UsersController
     end
 
     def create
+        @user = User.find(params[:user_id])
         @transaction = Transaction.new(transaction_params)
-        if @transaction.save
-            flash[:success] = "New Account Opened"
-            return redirect_to user_url(@user.id)
+        @account = @user.accounts.find(params[:transaction][:sender_id])
+        if User.find_by(username: :receiver_name).accounts.find_by(account_name: :receiver_act)
+            @receiver = User.find_by(username: :receiver_name)
+            @receiver_act = @receiver.accounts.find_by(account_name: :receiver_act)
+            if @account.balance >= @transaction.amount
+                if @transaction.save
+                    @account.balance = @account.balance - @transaction.amount
+                    @receiver_act.balance = @receiver_act.balance + @transaction.amount
+                    flash[:success] = "New Account Opened"
+                    return redirect_to user_url(@user.id)
+                else
+                    flash[:error] = @transaction.errors.full_messages
+                    render 'new'
+                end
+                
+            else
+                flash[:error] = "Insufficient Funds"
+            end
+            
         else
-            flash[:error] = @transaction.errors.full_messages
-            render 'new'
+            flash[:error] = "Incorrect User or Account"
         end
+        
     end
 
     private
