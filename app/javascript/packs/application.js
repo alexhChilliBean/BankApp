51192766a491a79ce5d09a96de94d492b3c476a9
Alex Holmes <alexholmes@Alexs-MacBook-Pro.local>
2021-10-28 17:24:21 +0100

    Allow floats as balance, finished css on new account page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,12 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import $ from 'jquery'
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-// Basic example
-$(document).ready( function () {
-    $('#transactionTable').DataTable();
-} );
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   #Session routes
   get '/login', to: 'sessions#login'
   post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
   delete '/logout', to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
