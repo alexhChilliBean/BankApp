b53dcb38323cb0b95a957c3096450a91e1d945ed
Alex Holmes <alexholmes@alexs-mbp.home>
2021-11-01 16:53:58 +0000

    Email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_many :accounts
     has_secure_password
-    validates :first_name, :last_name, :username, :password, presence: true
-    validates :username, uniqueness: true
+    validates :first_name, :last_name, :username, :password, :email presence: true
+    validates :username, :email uniqueness: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
 end
