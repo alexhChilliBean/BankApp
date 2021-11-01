e9b08b4c0c46fdcaf5c922a8b4ba59f1115ed4ae
Alex Holmes <alexholmes@alexs-mbp.home>
2021-11-01 17:11:14 +0000

    Welcome mail now sent when you create account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            UserMailer.welcome_email(@user).deliver_now
             session[:user_id] = @user.id
             flash[:success] = "Account Created"
             redirect_to root_path
