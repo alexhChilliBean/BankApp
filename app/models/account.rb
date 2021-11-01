2a12889fb3ed7447aa9ab84b3a2742ee9a5c239f
Alex Holmes <alexholmes@alexs-mbp.home>
2021-11-01 16:52:52 +0000

    Added emails column to users db

--- app/models/account.rb
+++ app/models/account.rb
@@ -8,3 +8,4 @@ class Account < ApplicationRecord
     validates :balance, numericality: {greater_than_or_equal_to: 0}
 
 end
+ 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_01_161312) do
+ActiveRecord::Schema.define(version: 2021_11_01_165154) do
 
   create_table "accounts", force: :cascade do |t|
     t.integer "account_no"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2021_11_01_161312) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "email"
   end
 
   add_foreign_key "accounts", "users"
