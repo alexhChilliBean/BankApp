0ad486bee1778ddf1da07982a3f2786408a1fe08
Alex Holmes <alexholmes@alexs-mbp.home>
2021-11-02 09:44:16 +0000

    Deactivated mailer for project

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
     def welcome_email(user)
-        @user=(user)
-        mail(to: @user.email, subject 'Welcome to BankApp!')
+        @user=user
+        mail(to: @user.email, subject: 'Welcome to BankApp!')
     end
 end
